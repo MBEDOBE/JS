68607532a1400fc3a0474ee175929650df127ee8
D. Mbedobe <danielbushbaby@gmail.com>
2022-06-30 13:21:46 +0000

    basic js

---
+++ 01-Fundamentals-1/eval.js
@@ -0,0 +1,11 @@
+let country = 'Ghana';
+let continent = 'Africa';
+let population = '24million';
+
+//output a
+console.log(country, continent, population);
+
+//output b
+console.log(country);
+console.log(population);
+console.log(continent);
---
+++ 01-Fundamentals-1/script.js
@@ -0,0 +1,40 @@
+let js = 'Mbedobe on 100-days of code';
+
+console.log(30 + 20 + 15 - 8);
+
+//values are just a piece of data
+
+('daniel'); //is a value
+
+// declare a variable
+
+let lastName = 'KUNJI'; // lastName is the variable
+
+//output in browser
+console.log(lastName);
+
+//boolean
+let learnJavaScript = true;
+console.log(learnJavaScript);
+
+//use typeof to det. the type of the value
+// console.log(typeof true);
+console.log(typeof learnJavaScript);
+// console.log(typeof 45);
+// console.log(typeof 'Ngeebi');
+
+//dynamic typing
+learnJavaScript = 'Yes!';
+console.log(typeof learnJavaScript);
+
+// undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+// dynamic
+year = 1991;
+console.log(typeof year);
+
+// typeof bug not corrected for legacy reasons
+console.log(typeof null); //reports null as object instead of null just like in undefined
